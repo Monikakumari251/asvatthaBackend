30d4223ec251d644a6ad14e2bba585f7df1a75fa
Monikakumari251 <monika.kumari@neosoftmail.com>
2021-06-05 16:21:17 +0530

    payment api

--- app/controllers/payment.controller.js
+++ app/controllers/payment.controller.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const Razorpay = require("razorpay");
 
+const Payment = require("../models/payment.model");
+
 // const router = express.Router();
 
 exports.post = async(req, res) => {
-    console.log('%%%%%%%%%%%%%%%%', process.env.RAZORPAY_KEY_ID);
-    
     try {
         const instance = new Razorpay({
             key_id: process.env.RAZORPAY_KEY_ID,
@@ -29,25 +29,37 @@ exports.post = async(req, res) => {
     }
   };
 
-// exports.post("/orders", async (req, res) => {
-//     try {
-//         const instance = new Razorpay({
-//             key_id: process.env.RAZORPAY_KEY_ID,
-//             key_secret: process.env.RAZORPAY_SECRET,
-//         });
-
-//         const options = {
-//             amount: 50000, // amount in smallest currency unit
-//             currency: "INR",
-//             receipt: "receipt_order_74394",
-//         };
-
-//         const order = await instance.orders.create(options);
-
-//         if (!order) return res.status(500).send("Some error occured");
-
-//         res.json(order);
-//     } catch (error) {
-//         res.status(500).send(error);
-//     }
-// });
+  // Create and Save a new Customer
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+  
+    // Create a Customer
+    const payment = new Payment({
+        name: req.body.name,
+        mobile: req.body.mobile,
+        village: req.body.village,
+        district: req.body.district,
+        city: req.body.city,
+        state: req.body.state,
+        email: req.body.email,
+        address: req.body.address,
+        aadhar: req.body.aadhar,
+        amount: req.body.amount,
+        pan_card: req.body.pan_card
+    });
+  
+    // Save Customer in the database
+    Payment.create(payment, (err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Customer."
+        });
+      else res.send(data);
+    });
+  };
---
+++ app/models/payment.model.js
@@ -0,0 +1,43 @@
+const sql = require("./db.js");
+
+// constructor
+const Payment =function(payment) {
+  this.name = volunteer.name;
+  this.mobile = volunteer.mobile;
+  this.village = volunteer.village;
+  this.email = volunteer.email;
+  this.district = volunteer.district;
+  this.pan_card = volunteer.pan_card;
+  this.city = volunteer.city;
+  this.state = volunteer.state;
+  this.address = volunteer.address;
+  this.aadhar = volunteer.aadhar;
+  this.amount = volunteer.amount;
+};
+
+// const Payment =function(payment) {
+//     name: req.body.name,
+//     mobile: req.body.mobile,
+//     village: req.body.village,
+//     district: req.body.district,
+//     city: req.body.city,
+//     state: req.body.state,
+//     email: req.body.email,
+//     address: req.body.address,
+//     aadhar: req.body.aadhar,
+//     amount: req.body.amount,
+//     pan_card: req.body.pan_card
+// };
+
+Payment.create = (newCustomer, result) => {
+  sql.query("INSERT INTO tbl_payment SET ?", newCustomer, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created payment id: ", { id: res.insertId, ...newCustomer });
+    result(null, { id: res.insertId, ...newCustomer });
+  });
+};
